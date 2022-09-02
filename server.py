9cffbcd005e0b334fee71997c034e590b3e21127
Monyjenni <chansovanmonyyoeun03@gmail.com>
2022-09-02 12:40:31 +0700

    init

---
+++ server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#server 
+import socket
+host = '172.16.0.209'        # Symbolic name meaning all available interfaces
+port = 5052     # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+print (host , port)
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+
+    try:
+        data = conn.recv(1024)
+
+        if not data: break
+
+        print ("Client Says: "+str(data))
+        conn.sendall(b'Server Says:hi')
+
+    except socket.error:
+        print ("Error Occured.")
+        break
+
+conn.close()
